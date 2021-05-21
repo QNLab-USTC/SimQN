4aa3b831b6887a4c4e214aaf4217f7951eb0a53f
ertuil <14366322+ertuil@users.noreply.github.com>
2021-05-21 19:49:32 +0800

    add autodoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,9 @@ html_baseurl = "/QuantNetSim"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+# import sphinx.ext.autodoc
 extensions = [
+    "sphinx.ext.autodoc"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
