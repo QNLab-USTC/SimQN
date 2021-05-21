741f06900f8c6b766b8305cff04ac656a426381d
ertuil <14366322+ertuil@users.noreply.github.com>
2021-05-21 09:47:00 +0800

    change to 0.0.2

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='qns',
     author='elliot',
-    version='0.1',
-    description='A quantum network simulator',
+    version='0.2',
+    description='A discrete time scheduler designed for Quantum Network',
     packages=find_packages(),
     include_package_data=True,
     exclude_package_data={'docs': ['.gitkeep']},
