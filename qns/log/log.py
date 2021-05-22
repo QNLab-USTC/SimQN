712c4514357f665fba1eaa11c2d568fe28037e41
ertuil <14366322+ertuil@users.noreply.github.com>
2021-05-22 21:30:20 +0800

    add docs to log, timer module

--- qns/log/log.py
+++ qns/log/log.py
@@ -5,6 +5,12 @@ from qns.schedular.simulator import Simulator
 
 
 class Log():
+    '''
+    ``Log`` is a simple log module to generate debug or statistics log.
+
+    :param str filename: the log will be output to ``filename`` file.
+    :param bool debug: the initial ``debug`` mode. 
+    '''
     def __init__(self, filename=None, debug=False):
         self.simulator = None
         self.filename = filename
@@ -15,58 +21,102 @@ class Log():
         self.file.close()
 
     def install(self, simulator: Simulator):
+        '''
+        ``install`` must be run to inject this log object into simulator.
+
+        :param simulator: the simulator
+        '''
         self.simulator = simulator
         self.simulator.log = self
 
-    def current_time(self):
+    def _current_time(self):
         if self.simulator.status == "run":
             return "[ {:10.8f} ]\t".format(self.simulator.current_time)
         else:
             return "[ {:10} ]\t".format(self.simulator.status)
 
-    def exp_time(self):
+    def _exp_time(self):
         if self.simulator.status == "run":
             return "{:},\t".format(self.simulator.current_time)
         else:
             return self.simulator.status+","
 
     def set_debug(self, debug: bool):
+        '''
+        Set the ``debug`` mode
+
+        :param bool debug: output debug log or not
+        '''
         self.is_debug = debug
 
     def set_file(self, filename=None):
+        '''
+        Write log into ``filename``. If ``filename`` is ``None``, ``sys.stdout`` will be used.
+
+        :param str filename: output filename
+        '''
         if filename is None:
             self.file = sys.stdout
         else:
             self.file = open(filename, 'w')
 
     def info(self, fmt: str, *args):
-        self.file.write(self.current_time())
+        '''
+        write ``info`` level log
+
+        :param str fmt: the format string
+        :param \*args: the string's parameters
+        '''
+        self.file.write(self._current_time())
         self.file.write("[info]\t")
         self.file.write(fmt.format(*args))
         self.file.write("\n")
 
     def warn(self, fmt: str, *args):
-        self.file.write(self.current_time())
+        '''
+        write ``warn`` level log
+
+        :param str fmt: the format string
+        :param \*args: the string's parameters
+        '''
+        self.file.write(self._current_time())
         self.file.write("[warn]\t")
         self.file.write(fmt.format(*args))
         self.file.write("\n")
 
     def error(self, fmt: str, *args):
-        self.file.write(self.current_time())
+        '''
+        write ``error`` level log
+
+        :param str fmt: the format string
+        :param \*args: the string's parameters
+        '''
+        self.file.write(self._current_time())
         self.file.write("[error]\t")
         self.file.write(fmt.format(*args))
         self.file.write("\n")
 
     def debug(self, fmt: str, *args):
+        '''
+        write ``debug`` level log. If ``debug`` is set to ``False``, the log will not be written.
+
+        :param str fmt: the format string
+        :param \*args: the string's parameters
+        '''
         if self.is_debug:
-            self.file.write(self.current_time())
+            self.file.write(self._current_time())
             self.file.write("[debug]\t")
             self.file.write(fmt.format(*args))
             self.file.write("\n")
 
-    # used for experiment output
     def exp(self, fmt: str, *args):
-        self.file.write(self.exp_time())
+        '''
+        Write log in csv format. It can be used to output experiment data.
+
+        :param str fmt: the format string
+        :param \*args: the string's parameters
+        '''
+        self.file.write(self._exp_time())
         self.file.write(fmt.format(*args))
         self.file.write("\n")
 
--- qns/timer/timer.py
+++ qns/timer/timer.py
@@ -8,6 +8,9 @@ class TimerEvent(Event):
         self.timer = timer
 
     def run(self, simulator: Simulator):
+        '''
+        This is an inner event that triggle ``Timer``'s ``run`` function.
+        '''
         self.timer.run(simulator)
 
 
@@ -16,11 +19,29 @@ class TimerAllocEvent(Event):
         self.timer = timer
 
     def run(self, simulator: Simulator):
+        '''
+        This is an inner event that triggle ``Timer``'s ``alloc`` function.
+        '''
         self.timer.alloc(simulator)
 
 
 class Timer(Entity):
-    def __init__(self, start_time, end_time, step_time, alloc_time=None, name=None):
+    '''
+    Timer is a virtual entity that triggle a ``TimerEvent`` ine-time or periodly.
+
+    If both ``step_time`` and ``end_time`` is not ``None``, the timer will call ``TimerEvent`` periodly.
+    Or it will only triggle once at ``start_time``.
+
+    There will be a great overhead if every ``TimerEvent`` is generated before the simulator runs.
+    Setting an appropriate ``alloc_time`` can effectively reduce the initialization time.
+
+    :param float start_time: The timer's start time in second
+    :param float end_time: The timer's finish time in second
+    :param float step_time: The ``TimerEvent`` will be called every ``step_time``
+    :param float alloc_time: The next ``TimerEvent`` will be generated and inserted into simulator every ``alloc_time``
+    :param str name: timer's name
+    '''
+    def __init__(self, start_time, end_time = None , step_time = None, alloc_time=1, name=None):
         self.step_time = step_time
         self.start_time = start_time
         self.end_time = end_time
@@ -31,6 +52,9 @@ class Timer(Entity):
             self.name = name
 
     def install(self, simulator: Simulator):
+        '''
+        This function runs before simulator is started. It is used to inject initial ``TimerEvent``s.
+        '''
         if self.step_time is not None:
             self.step_time_slice = simulator.to_time_slice(self.step_time)
         if self.end_time is not None:
@@ -50,6 +74,11 @@ class Timer(Entity):
             simulator.add_event(self.start_time_slice, TimerEvent(self))
 
     def run(self, simulator: Simulator):
+        '''
+        This function must be overrided to perform specific functions.
+
+        :param simulator: the simulator
+        '''
         raise NotImplemented
 
     def alloc(self, simulator: Simulator):
