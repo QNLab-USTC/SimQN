95b6dc5cdfcfed74ba242996156e28f1fecd01d3
Elliot Chen <elliot.98@outlook.com>
2022-01-12 15:01:15 +0800

    [license] update license to GPLv3

--- setup.py
+++ setup.py
@@ -1,3 +1,20 @@
+#    SimQN: a discrete-event simulator for the quantum networks
+#    Copyright (C) 2021-2022 Lutong Chen, Jian Li, Kaiping Xue
+#    University of Science and Technology of China, USTC.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
@@ -18,7 +35,7 @@ setup(
     install_requires=["numpy"],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
 )
